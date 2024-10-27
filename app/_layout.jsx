ce89c3246c223459fc34d46fee5d79123039a33e
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2024-10-28 00:41:03 +0530

    assests, fonts added, nativewind added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,27 @@
-import { Slot, Stack } from "expo-router";
+import { SplashScreen, Slot, Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
 
 // Import your global CSS file
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
+  const { fontsLoaded, error } = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
   // return (
   //   <>
   //     <SafeAreaView style={styles.container}>
@@ -18,9 +33,16 @@ const RootLayout = () => {
   //     </SafeAreaView>
   //   </>
   // );
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown : false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 };
@@ -31,7 +53,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: "#fff",
-      alignItems: "center",
+    alignItems: "center",
     justifyContent: "center",
   },
 });
