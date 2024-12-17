c80b29063a56adafeb14dde15391142060bec180
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2024-12-17 15:42:43 +0530

    feat: implement Trending component with video playback functionality; add expo-video dependency

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,17 +1,102 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
 import EmptyState from "./EmptyState";
+import * as Animatable from "react-native-animatable";
+import { icons } from "../constants";
+import { ResizeMode, Video } from "expo-av";
+
+//TODO start from importing Video from expo-video
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1.1,
+  },
+};
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+  // console.log("bal: ", item.video);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem.video === item.video ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Video
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+          onError={(error) => {
+            console.error("Video Error: ", error);
+          }}
+        />
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => {
+            setPlay(true);
+          }}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].item);
+    }
+  };
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.video}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white" key={item.$id}>
-          {item.$id}
-        </Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{ itemVisiblePercentThreshold: 70 }} //50% of the item must be visible to be considered as in view
+      contentOffset={{ x: 170 }} //initial offset from the left
       horizontal
       showsHorizontalScrollIndicator={false} //hides the horizontal scrollbar
     />
